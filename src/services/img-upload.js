3535f85cac82a32968743fcbce82dbabb676ce37
Isabel Mill <isabelmavd2@gmail.com>
2022-07-19 00:37:48 +0300

    env front

--- src/services/img-upload.js
+++ src/services/img-upload.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 // AXIOS
 export const uploadImg = async (file) => {
-    const UPLOAD_PRESET = 'cajan_2022';
-    const CLOUD_NAME = 'dw85wdwsw'; 
+    const UPLOAD_PRESET = process.env.REACT_APP_UPLOAD_PRESET;
+    const CLOUD_NAME = process.env.REACT_APP_CLOUD_NAME; 
     const UPLOAD_URL = `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/image/upload`;
     const FORM_DATA = new FormData();
     FORM_DATA.append('file', file);
